696b5edac7667e1022593ef47a2ff609af36f8fc
stintosestudios <stintose.studios@gmail.com>
2017-01-09 20:10:29 -0500

    new project

---
+++ projects/crab/scene.js
@@ -0,0 +1,116 @@
+
+scene({
+
+    maxFrame : 100,
+
+    viewPort : {
+
+        w : 480,
+        h : 360
+
+    },
+
+    logo : {
+        w : 128,
+        h : 56,
+        opacity : .4,
+        skin : {
+            imgIndex : 0,
+            sx : 0,
+            sy : 0,
+            sw : 128,
+            sh : 56
+        }
+    },
+
+    // define some parts
+    parts : [{
+
+            id : 'lower_center',
+            w : 256,
+            h : 32,
+
+            forFrame : function (pt) {
+
+                var bias = Math.abs(.5 - this.percentDone) / .5;
+
+                pt.x = this.viewPort.w / 2 - pt.w / 2;
+                pt.y = this.viewPort.h / 2 - pt.h / 2;
+                pt.radian = .2 - .4 * bias;
+
+            },
+            skin : {
+
+                appendRender : function (ctx, skin) {
+
+                    ctx.strokeStyle = '#00ffff';
+                    ctx.strokeRect(0, 0, skin.part.w, skin.part.h);
+
+                }
+
+            }
+
+        }, {
+
+            id : 'lower_left',
+            w : 32,
+            h : 128,
+
+            forFrame : function (pt) {
+
+                var bias = Math.abs(.5 - this.percentDone) / .5,
+                radian = .2 - .4 * bias,
+                cx = this.viewPort.w / 2 - pt.w / 2,
+                cy = this.viewPort.h / 2 - pt.h / 2;
+
+                pt.x = cx + Math.cos(radian) * 128;
+                pt.y = cy + Math.sin(radian) * 128 + 64;
+
+            },
+            skin : {
+
+                appendRender : function (ctx, skin) {
+
+                    ctx.strokeStyle = '#00ffff';
+                    ctx.strokeRect(0, 0, skin.part.w, skin.part.h);
+
+                }
+
+            }
+
+        },
+
+    ],
+
+    // define the forFrame movement
+    forFrame : function () {}
+
+});
+
+// inject a canvas into an element with an id of 'apparea'.
+scene.injectCanvas('apparea');
+
+scene.load(
+    [
+        '../mylogo_128.png'
+    ],
+    function (progress) {
+
+    // uncomment to save as png
+    if (progress === 1) {
+
+        var playback = {
+
+            appendRender : function (ctx) {},
+            appendZ : 0,
+
+            containerId : 'apparea',
+
+            frameRate : 40
+        };
+
+        scene.injectUI(playback);
+
+    }
+
+});
